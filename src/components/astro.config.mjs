6aee5db327d1b484ca08769327bff9ebdacf105b
josholiv <joshuamolivier@gmail.com>
2025-02-04 21:25:21 -0600

    update

--- src/components/astro.config.mjs
+++ src/components/astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-
-import preact from "@astrojs/preact";
-
+import preact from '@astrojs/preact';
+// https://astro.build/config
 export default defineConfig({
-  site: "https://josholivier.netlify.app/",
   integrations: [preact()]
 });
