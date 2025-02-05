20240302ee3be04ef483e3cda296d447128810d9
josholiv <joshuamolivier@gmail.com>
2025-02-04 20:33:14 -0600

    update

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Josh | Blog',
+    title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
