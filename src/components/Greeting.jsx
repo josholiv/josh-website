9990e363c92f77bf7721b99b422fa7e7ce020a5b
josholiv <joshuamolivier@gmail.com>
2025-02-13 13:28:35 -0600

    Update Greeting.jsx to randomize on ititial page load

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -4,8 +4,8 @@ export default function Greeting({messages}) {
 
   const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
 
-  const [greeting, setGreeting] = useState(messages[0]);
-
+  const [greeting, setGreeting] = useState(() => randomMessage());
+  
   return (
     <div>
       <h3>{greeting}! Thank you for visiting!</h3>
