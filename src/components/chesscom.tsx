82af59cb1e70abe8c6109420133936dc14057083
josholiv <joshuamolivier@gmail.com>
2025-02-26 17:05:40 -0600

    update chess stats with Magnus Carlsen stats

--- src/components/chesscom.tsx
+++ src/components/chesscom.tsx
@@ -7,46 +7,56 @@ const fetchChessStats = async (username: string) => {
 };
 
 const username = 'pichugang';
+const magnusUsername = 'MagnusCarlsen';
 
 const ChessStats: FunctionalComponent = () => {
   const [stats, setStats] = useState(null);
+  const [magnusStats, setMagnusStats] = useState(null);
 
   useEffect(() => {
     const getStats = async () => {
-      const data = await fetchChessStats(username);
-      setStats(data);
+      const [userStats, magnusStats] = await Promise.all([
+        fetchChessStats(username),
+        fetchChessStats(magnusUsername),
+      ]);
+      setStats(userStats);
+      setMagnusStats(magnusStats);
     };
 
     getStats();
   }, []);
 
-  // Check if stats have been loaded before rendering
-  if (!stats) {
+  if (!stats || !magnusStats) {
     return <div>Loading...</div>;
   }
 
+  const getEloDifference = (category: string) => {
+    const myElo = stats[category]?.last?.rating || 0;
+    const magnusElo = magnusStats[category]?.last?.rating || 0;
+    return magnusElo - myElo;
+  };
+
   return (
     <div>
-      <h1>Josh's Chess.com Stats</h1>
+      <h1>My Chess.com Stats♟️</h1>
 
       <h2>Rapid</h2>
-      <p>Last Rating: {stats.chess_rapid?.last?.rating}</p>
+      <p>Rating: <strong>{stats.chess_rapid?.last?.rating}</strong>. Only {getEloDifference('chess_rapid')} behind Magnus Carlsen!</p>
       <p>Best Rating: {stats.chess_rapid?.best?.rating}</p>
       <p>Record: {stats.chess_rapid?.record?.win} wins, {stats.chess_rapid?.record?.loss} losses</p>
 
       <h2>Bullet</h2>
-      <p>Last Rating: {stats.chess_bullet?.last?.rating}</p>
+      <p>Rating: <strong>{stats.chess_bullet?.last?.rating}</strong>. Only {getEloDifference('chess_bullet')} behind Magnus Carlsen!</p>
       <p>Best Rating: {stats.chess_bullet?.best?.rating}</p>
       <p>Record: {stats.chess_bullet?.record?.win} wins, {stats.chess_bullet?.record?.loss} losses</p>
 
       <h2>Blitz</h2>
-      <p>Last Rating: {stats.chess_blitz?.last?.rating}</p>
+      <p>Rating: <strong>{stats.chess_blitz?.last?.rating}</strong>. Only {getEloDifference('chess_blitz')} behind Magnus Carlsen!</p>
       <p>Best Rating: {stats.chess_blitz?.best?.rating}</p>
       <p>Record: {stats.chess_blitz?.record?.win} wins, {stats.chess_blitz?.record?.loss} losses</p>
 
       <h2>Puzzle Rush</h2>
       <p>Best Score: {stats.puzzle_rush?.best?.score}</p>
-      <p>Total Attempts: {stats.puzzle_rush?.best?.total_attempts}</p>
     </div>
   );
 };
