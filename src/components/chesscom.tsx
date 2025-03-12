cc594c867e946bd29176bc420e731bdd989a4ae7
josholiv <joshuamolivier@gmail.com>
2025-03-12 16:50:06 -0500

    update stats page stuff to clean it up

--- src/components/chesscom.tsx
+++ src/components/chesscom.tsx
@@ -38,26 +38,11 @@ const ChessStats: FunctionalComponent = () => {
 
   return (
     <div>
-      <h3>My chess stats♟️</h3>
-      <h4>(data from chess.com)</h4>
-
-      <h4>Rapid</h4>
-      <p>Rating: <strong>{stats.chess_rapid?.last?.rating}</strong>. Only {getEloDifference('chess_rapid')} behind Magnus Carlsen!</p>
-      <p>Best Rating: {stats.chess_rapid?.best?.rating}</p>
-      <p>Record: {stats.chess_rapid?.record?.win} wins, {stats.chess_rapid?.record?.loss} losses</p>
-
-      <h4>Bullet</h4>
-      <p>Rating: <strong>{stats.chess_bullet?.last?.rating}</strong>. Only {getEloDifference('chess_bullet')} behind Magnus Carlsen!</p>
-      <p>Best Rating: {stats.chess_bullet?.best?.rating}</p>
-      <p>Record: {stats.chess_bullet?.record?.win} wins, {stats.chess_bullet?.record?.loss} losses</p>
-
-      <h4>Blitz</h4>
-      <p>Rating: <strong>{stats.chess_blitz?.last?.rating}</strong>. Only {getEloDifference('chess_blitz')} behind Magnus Carlsen!</p>
-      <p>Best Rating: {stats.chess_blitz?.best?.rating}</p>
-      <p>Record: {stats.chess_blitz?.record?.win} wins, {stats.chess_blitz?.record?.loss} losses</p>
-
-      <h4>Puzzle Rush</h4>
-      <p>Best Score: {stats.puzzle_rush?.best?.score}</p>
+      <h3>Bullet chess♟️ (data from chess.com)</h3>
+        <h4>Bullet</h4>
+          <p>Rating: <strong>{stats.chess_bullet?.last?.rating}</strong>. Only {getEloDifference('chess_bullet')} behind Magnus Carlsen!</p>
+          <p>Best Rating: {stats.chess_bullet?.best?.rating}</p>
+          <p>Record: {stats.chess_bullet?.record?.win} wins, {stats.chess_bullet?.record?.loss} losses</p>
     </div>
   );
 };
