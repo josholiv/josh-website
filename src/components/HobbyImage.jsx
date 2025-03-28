a376ef35456c4aa43c92a898fe55c8f8144ea7c0
josholiv <joshuamolivier@gmail.com>
2025-03-28 11:10:20 -0500

    updates to about.astro bio text and formatting, testing update to triathlon stats formatting

--- src/components/HobbyImage.jsx
+++ src/components/HobbyImage.jsx
@@ -6,7 +6,6 @@ const HobbyImage = ({ hobbies }) => {
   const hobbyImages = {
     "Tereré 🧉": "/images/hobbies/terere.jpg",
     "3D printing ⚙️": "/images/hobbies/3d-printing.jpg",
-    "Neuroscience 🧠": "/images/hobbies/neuroscience.jpg",
     "Yoyo tricks 🪀": "/images/hobbies/yoyo-tricks.jpg",
     "Triathlons 🏊🚴🏃‍♂️": "/images/hobbies/triathlon.jpg",
   };
@@ -17,22 +16,33 @@ const HobbyImage = ({ hobbies }) => {
 
   return (
     <div>
-    <h3>Some things I love are:</h3>
-    <div style={{ display: "flex", alignItems: "center", gap: "2rem", width: "100%" }}>
-      <ul style={{ fontSize: "1.2rem", listStyle: "square", margin: 0, padding: "1rem", flexShrink: 0 }}>
-        {hobbies.map((hobby) => (
-          <li
-            key={hobby}
-            style={{
-              cursor: "pointer",
-              color: selectedHobby === hobby ? "#41ab5d" : "inherit",
-            }}
-            onClick={() => handleHobbyChange(hobby)}
-          >
-            <strong>{hobby}</strong>
-          </li>
-        ))}
-      </ul>
+   <div style={{ display: "flex", alignItems: "center", gap: "2rem", width: "100%" }}>
+  <div style={{ width: "100%" }}>
+    <p style={{ margin: 0 }}>Some of my favorite things are:</p>
+    <ul
+      style={{
+        fontSize: "1.2rem",
+        listStyle: "square",
+        margin: 0,
+        padding: "1rem",
+        flexShrink: 0,
+        paddingLeft: "2rem", // Indentation applied to the list only
+      }}
+    >
+      {hobbies.map((hobby) => (
+        <li
+          key={hobby}
+          style={{
+            cursor: "pointer",
+            color: selectedHobby === hobby ? "#41ab5d" : "inherit",
+          }}
+          onClick={() => handleHobbyChange(hobby)}
+        >
+          <strong>{hobby}</strong>
+        </li>
+      ))}
+    </ul>
+  </div>
 
       {selectedHobby && hobbyImages[selectedHobby] && (
         <img
@@ -40,10 +50,10 @@ const HobbyImage = ({ hobbies }) => {
           alt={selectedHobby}
           style={{
             flex: "1 1 50%", // Grow, shrink, and start at 50% width
-            maxWidth: "50vw", // Prevents the image from being too wide
-            minWidth: "10rem", // Prevents it from shrinking too much
+            maxWidth: "50rem", // Prevents the image from being too wide
+            minWidth: "8rem", // Prevents it from shrinking too much
             height: "auto", // Maintains aspect ratio
-            borderRadius: "1rem",
+            borderRadius: "50%",
             objectFit: "contain", // Ensures the image fits well
           }}
         />
--- src/components/TriathlonStats.jsx
+++ src/components/TriathlonStats.jsx
@@ -30,37 +30,39 @@ const TriathlonStats = ({ data, error }) => {
     <div>
       <h3>During my triathlon training* in <strong>{new Date().getFullYear()}</strong>, I've gone:</h3>
       {!error && (
-        <p style={{ paddingLeft: '2rem' }}>
-        <strong style={{ fontSize: '2rem', color: '#0099cc' }}>
-          🏊 {unit === "miles" ? formatNumber(data.swimDistance) : 
-              unit === "km" ? formatNumber(swimDistanceKm) : 
-              unit === "yards" ? formatNumber(convertToYards(swimDistanceKm * 1000)) : 
-              unit === "fields" ? formatNumber(convertToFields(swimDistanceKm * 1000)) : 
-              convertToMoonPercentage(swimDistanceKm)}
-        </strong> 
-        {unit === "miles" ? " miles" : 
-         unit === "km" ? " kilometers" : 
-         unit === "yards" ? " yards" : 
-         unit === "fields" ? " football fields🏈" : 
-         "% of the distance from Earth🌎 to the Moon🌕"} swimming
+         <p style={{ paddingLeft: '2rem' }}>
+         <strong style={{ fontSize: '2rem', color: '#0099cc' }}>
+            🏊 {unit === "miles" ? formatNumber(data.swimDistance) + " " : 
+                unit === "km" ? `${formatNumber(swimDistanceKm)} kilometers`  : 
+                unit === "yards" ? `${formatNumber(convertToYards(swimDistanceKm * 1000))} yards`  : 
+                unit === "fields" ? `${formatNumber(convertToFields(swimDistanceKm * 1000))} football fields` :
+                `${convertToMoonPercentage(swimDistanceKm)}%`}
+          </strong>
+          {unit === "fields" ? " 🏈" : ""}
+          {unit !== "fields" && unit !== "miles" && unit !== "km" && unit !== "yards" ? " of the distance from Earth🌎 to the Moon🌕" : ""}
+          swimming<br />
       
-      
-
           <strong style={{ fontSize: '2rem', color: '#41ab5d' }}>
             🚴 {unit === "miles" ? formatNumber(data.rideDistance) + " " : 
-                unit === "km" ? `${formatNumber(rideDistanceKm)} kilometers ` : 
-                unit === "yards" ? `${formatNumber(convertToYards(rideDistanceKm * 1000))} yards ` : 
-                unit === "fields" ? `${formatNumber(convertToFields(rideDistanceKm * 1000))} football fields🏈 ` :
-                `${convertToMoonPercentage(rideDistanceKm)}% of the distance from Earth🌎 to the Moon🌕`} 
-          </strong> biking<br />
+                unit === "km" ? `${formatNumber(rideDistanceKm)} kilometers` : 
+                unit === "yards" ? `${formatNumber(convertToYards(rideDistanceKm * 1000))} yards` : 
+                unit === "fields" ? `${formatNumber(convertToFields(rideDistanceKm * 1000))} football fields` :
+                `${convertToMoonPercentage(rideDistanceKm)}%`}
+          </strong>
+          {unit === "fields" ? " 🏈" : ""}
+          {unit !== "fields" && unit !== "miles" && unit !== "km" && unit !== "yards" ? " of the distance from Earth🌎 to the Moon🌕" : ""}
+          biking<br />
 
           <strong style={{ fontSize: '2rem', color: '#ffaa00' }}>
             🏃‍♂️ {unit === "miles" ? formatNumber(data.runDistance) + " " : 
-                unit === "km" ? `${formatNumber(runDistanceKm)} kilometers ` : 
-                unit === "yards" ? `${formatNumber(convertToYards(runDistanceKm * 1000))} yards ` : 
-                unit === "fields" ? `${formatNumber(convertToFields(runDistanceKm * 1000))} football fields🏈 ` :
-                `${convertToMoonPercentage(runDistanceKm)}% of the distance from Earth🌎 to the Moon🌕`} 
-          </strong> running
+                unit === "km" ? `${formatNumber(runDistanceKm)} kilometers` : 
+                unit === "yards" ? `${formatNumber(convertToYards(runDistanceKm * 1000))} yards` : 
+                unit === "fields" ? `${formatNumber(convertToFields(runDistanceKm * 1000))} football fields` :
+                `${convertToMoonPercentage(runDistanceKm)}%`}
+          </strong>
+          {unit === "fields" ? " 🏈" : ""}
+          {unit !== "fields" && unit !== "miles" && unit !== "km" && unit !== "yards" ? " of the distance from Earth🌎 to the Moon🌕" : ""}
+          running
         </p>
       )}
       
