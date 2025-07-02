21089dd156163ceef4882b822c6f1c20b5c10701
josholiv <joshuamolivier@gmail.com>
2025-07-02 15:41:21 -0500

    added animated brain and fixed some issues with theme toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,25 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-import expressiveCode from "astro-expressive-code"; // expressive code for markdown code block themes
+import expressiveCode from "astro-expressive-code";
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: "https://josholivier.netlify.app/",
-  integrations: [preact(), expressiveCode({
-    themes: ['kanagawa-dragon'],
-  })],
-    }, mdx(),
-);
+  integrations: [
+    preact(),
+    expressiveCode({
+      themes: ['kanagawa-dragon'],
+    }),
+    mdx()
+  ],
+  vite: {
+    optimizeDeps: {
+      include: ['three', 'three/examples/jsm/loaders/STLLoader'],
+    },
+    resolve: {
+      alias: {
+        three: 'three',
+      },
+    },
+  },
+});
