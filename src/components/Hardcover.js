d8cf2fa3363fb89b1158a6dd903ee2db6a1f6987
josholiv <joshuamolivier@gmail.com>
2025-10-11 17:10:15 -0500

    api call fix

--- src/components/Hardcover.js
+++ src/components/Hardcover.js
@@ -27,7 +27,7 @@ export default class Hardcover {
       {
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${env.HARDCOVER_API_KEY}`,
+          "Authorization": `${env.HARDCOVER_API_KEY}`,
         },
       }
     );
