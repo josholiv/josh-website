4c0f6a6e285efcbc17964db9064a59f619712c4e
josholiv <joshuamolivier@gmail.com>
2025-10-25 18:10:20 -0500

    update to font colors and styles

--- src/components/HardcoverStats.jsx
+++ src/components/HardcoverStats.jsx
@@ -20,7 +20,7 @@ const HardcoverStats = ({ data }) => {
       <div style={{ display: "flex", height: "100%" }}>
         {/* Books Read section */}
         <div style={{ flex: 1, textAlign: "left", paddingRight: "1rem"}}>
-          <div style={{ fontSize: "1rem", marginBottom: "0.3rem", color: "var(--neutral-400)" }}>
+          <div style={{ fontSize: "1rem", marginBottom: "0.3rem", color: "var(--neutral-200)" }}>
             Read
           </div>
           <div style={{ fontSize: "1.3rem", fontWeight: "bold" }}>{progress}</div>
@@ -31,7 +31,7 @@ const HardcoverStats = ({ data }) => {
 
         {/* Reading Goal section */}
         <div style={{ flex: 1, textAlign: "left", paddingLeft: "1rem"}}>
-          <div style={{ fontSize: "1rem", marginBottom: "0.3rem", color: "var(--neutral-400)" }}>
+          <div style={{ fontSize: "1rem", marginBottom: "0.3rem", color: "var(--neutral-200)" }}>
             Goal
           </div>
           <div style={{ fontSize: "1.3rem", fontWeight: "bold" }}>{goal}</div>
@@ -46,7 +46,7 @@ const HardcoverStats = ({ data }) => {
         style={{
           padding: "1rem",
           backgroundColor: "var(--neutral-800)",
-          color: "var(--neutral-400)",
+          color: "var(--neutral-200)",
           borderRadius: "5px",
           display: "flex",
           flexWrap: "wrap",
@@ -54,7 +54,7 @@ const HardcoverStats = ({ data }) => {
         }}
       >
         {goals.map((g, i) =>
-          formatCard(g.progress, g.goal, "var(--neutral-400)", i === goals.length - 1)
+          formatCard(g.progress, g.goal, "var(--neutral-200)", i === goals.length - 1)
         )}
       </div>
     </div>
--- src/components/chesscom.tsx
+++ src/components/chesscom.tsx
@@ -75,9 +75,9 @@ const ChessStats: FunctionalComponent = () => {
           justifyContent: 'left',
           gap: '1rem',
           }}>
-          {formatCard('Bullet', stats.chess_bullet?.last?.rating, 'var(--red-400)', false)}
-          {formatCard('Blitz', stats.chess_blitz?.last?.rating, 'var(--pink-400)', false)}
-          {formatCard('Rapid', stats.chess_rapid?.last?.rating, 'var(--purple-400)', true)}
+          {formatCard('Bullet', stats.chess_bullet?.last?.rating, 'var(--red-200)', false)}
+          {formatCard('Blitz', stats.chess_blitz?.last?.rating, 'var(--pink-200)', false)}
+          {formatCard('Rapid', stats.chess_rapid?.last?.rating, 'var(--purple-200)', true)}
         </div>
       </div>
       </div>
