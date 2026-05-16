3446fff0034ed8b683524d7f40258d07d21c6ec5
josholiv <joshuamolivier@gmail.com>
2026-05-16 11:42:55 -0500

    Remove unused Three.js STLLoader and related files from the project

--- .vite-cache/deps/@astrojs_preact_client__js.js
+++
@@ -1,9 +0,0 @@
-import {
-  client_default
-} from "./chunk-ZEIKIQYP.js";
-import "./chunk-V4UUEOLZ.js";
-import "./chunk-EJ5P4D46.js";
-import "./chunk-G3PMV62Z.js";
-export {
-  client_default as default
-};
--- .vite-cache/deps/astro___html-escaper.js
+++
@@ -1,9 +0,0 @@
-import {
-  escape,
-  unescape
-} from "./chunk-NMRFGD2L.js";
-import "./chunk-G3PMV62Z.js";
-export {
-  escape,
-  unescape
-};
